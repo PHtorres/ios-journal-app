24ab66bc6ee6a339da3ded425affcfbc6d4f99b2
Paulo Henrique Torres <paulohenriquetorresdev@gmail.com>
2025-09-16 17:19:50 -0300

    Initial Commit

---
+++ IOSJournalApp/IOSJournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSJournalAppApp.swift
+//  IOSJournalApp
+//
+//  Created by Paulo Henrique Torres on 16/09/25.
+//
+
+import SwiftUI
+
+@main
+struct IOSJournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
